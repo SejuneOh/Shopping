6763c1c21eced098874334153ceba7b558d2cca2
sejune-oh <etry0715@icloudhospital.com>
2023-11-16 18:44:20 +0900

    feat: Add properties in UserModel

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -3,5 +3,9 @@ namespace Shopping;
 
 public class UserModel
 {
+  public string Id { get; set; }
+  public string OrderId { get; set; }
+  public string name { get; set; }
+  public string? email { get; set; }
 
 }
