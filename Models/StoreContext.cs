5d6bba673552330536db85168521ecd0ad859ef4
SejuneOh <etry0715@gmail.com>
2023-11-17 00:20:48 +0900

    feat: Connet DB

---
+++ Models/StoreContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Shopping;
+
+public class StoreContext : DbContext
+{
+  // generator function
+  public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
+
+  public DbSet<UserModel> User { get; set; }
+  public DbSet<ProductsModel> Products { get; set; }
+  public DbSet<ProductItemModel> Product { get; set; }
+  public DbSet<OrderModel> Order { get; set; }
+}
